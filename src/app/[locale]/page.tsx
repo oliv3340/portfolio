15b6fcb55c3a5148561fef4885ad27526db9a244
Olivier Graciannette <olivier.graciannette#gmail.com>
2024-02-19 16:16:17 +0100

    feat: add footer and usefull links

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import NavBar from "@/components/navigation/NavBar";
 import { useTranslations } from "next-intl";
 
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <main className="min-w-screen min-h-screen">
       <NavBar />
+      <Footer />
     </main>
   );
 }
