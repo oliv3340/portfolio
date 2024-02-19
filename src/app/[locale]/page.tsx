1965875bb9e8aff9eb15983e3d4d725889b23b0a
Olivier Graciannette <olivier.graciannette#gmail.com>
2024-02-19 16:25:54 +0100

    fix: clean code

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,7 @@
 import Footer from "@/components/Footer";
 import NavBar from "@/components/navigation/NavBar";
-import { useTranslations } from "next-intl";
 
 export default function Home() {
-  const t = useTranslations('Home');
   return (
     <main className="min-w-screen min-h-screen">
       <NavBar />
