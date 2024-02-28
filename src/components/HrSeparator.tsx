dbc9f3c76ddebc3cd8a5ea11cdd148636cf48cf2
Olivier Graciannette <olivier.graciannette#gmail.com>
2024-02-28 22:11:00 +0100

    feat: Add HrSeparator component

---
+++ src/components/HrSeparator.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function HrSeparator() {
+    return (
+        <hr className="w-80 md:w-hrWidth h-hrHeight bg-lightBlack mx-auto my-8 border-0 rounded" />
+    )
+}
